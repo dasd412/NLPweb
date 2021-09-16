c62c3708e0c591f2b7cf845157b97735e4401242
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-09-16 17:37:25 +0900

    added doughnut chart

--- src/main/resources/static/js/app/charts/doughnut.js
+++ src/main/resources/static/js/app/charts/doughnut.js
@@ -1,23 +1,11 @@
 const config = {
   type: 'doughnut',
   data: {
-          labels: [
-            'Like',
-            'Disgusting',
-            'Interesting',
-            'Angry',
-            'Sad'
-          ],
+          labels: [],
           datasets: [{
-            label: 'My First Dataset',
-            data: [20,30,10,20,20],
-            backgroundColor: [
-               'rgba(54, 162, 235, 0.5)',
-               'rgba(255, 206, 86, 0.5)',
-               'rgba(75, 192, 192, 0.5)',
-               'rgba(153, 102, 255, 0.5)',
-               'rgba(255, 159, 64, 0.5)'
-            ],
+            label: '',
+            data: [],
+            backgroundColor: [],
             hoverOffset: 4
           }]
   },
@@ -42,3 +30,66 @@ const config = {
 const ctx = document.getElementById('chart').getContext('2d');
 const chart = new Chart(ctx, config);
 
+const backgroundColor = ['rgba(255, 99, 132, 0.5)',
+    'rgba(54, 162, 235, 0.5)',
+    'rgba(255, 206, 86, 0.5)',
+    'rgba(75, 192, 192, 0.5)',
+    'rgba(153, 102, 255, 0.5)',
+    'rgba(255, 159, 64, 0.5)'];
+
+function resetChart(chart){
+    chartArray=[];
+    chart.data.labels=[];
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data=[];
+        dataset.backgroundColor=[];
+        dataset.borderColor=[];
+    });
+}
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function drawBackgroundColor(chart, index) {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.backgroundColor.push(backgroundColor[index%6]);
+    });
+
+    chart.update();
+}
+
+
+function getComments(){
+  return JSON.parse(localStorage.getItem('comment'));
+}
+
+$(document).ready(function () {
+    comments=getComments();
+
+    const commentDict={};
+    for(let i=0;i<comments.length;i++){
+      commentDict[comments[i]]=0.0;
+    }
+
+    for(let i=0;i<comments.length;i++){
+       commentDict[comments[i]]+=1.0;
+    }
+
+    let idx=0;
+    for (let key in commentDict){
+      commentDict[key]=(commentDict[key]/comments.length)*100.0;
+      addData(chart,key,commentDict[key]);
+      idx+=1;
+    }
+    console.log('len',idx);
+
+    for(let i=0;i<idx;i++){
+      drawBackgroundColor(chart,i);
+    }
+
+});
