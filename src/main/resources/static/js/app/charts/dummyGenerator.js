cfc20f37c5ed0e9df4dafc1235a1f7b3ae737fe2
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-09-16 11:09:48 +0900

    completed data transfer and save into js localStorage.

--- src/main/resources/static/js/app/charts/dummyGenerator.js
+++ src/main/resources/static/js/app/charts/dummyGenerator.js
@@ -8,11 +8,31 @@ const generator={
     $('#randomSet').on('click',function(){
       _this.generateRandomData();
     });
-  },
+  },//init function
 
   generateRandomData : function(){
-    console.log('test function');
-  }
+    $.ajax({
+      url:"/api/nlp/charts/randomset",
+      type:'get',
+      dataType:'json',
+      contentType:'application/json;charset=utf-8'
+    }).done(function(data){
+      const e=data.response;
+
+      approvalRatings=[];
+      comment=[];
+
+      for(i=0;i<e.length;i++){
+        approvalRatings.push(e[i].approvalRating);
+        comment.push(e[i].comment);
+      }
+
+      localStorage.setItem('approvalRatings',JSON.stringify(approvalRatings));
+      localStorage.setItem('comment',JSON.stringify(comment));
+
+      //localComment=(localStorage.getItem('comment');
+    });
+  }//generate random data function
 
 };
 
