a0f19b15db0a89f90c4fb233903f52d8655593fa
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-10-02 16:50:37 +0900

    save test of article with reply success

--- src/main/java/com/dasd412/domain/article/ArticleRepository.java
+++ src/main/java/com/dasd412/domain/article/ArticleRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, String> {
 
     @Query(value = "from Article where date between :startDate and :endDate")
     List<Article> findAllBetweenDates(@Param("startDate") LocalDateTime startDate,
--- src/test/java/com/dasd412/domain/article/ArticleRepositoryTest.java
+++ src/test/java/com/dasd412/domain/article/ArticleRepositoryTest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import javax.transaction.Transactional;
 
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,28 +21,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArticleRepositoryTest {
 
     @Autowired
-    ArticleRepository repository;
+    ArticleRepository articleRepository;
+
+    @Autowired
+    ReplyRepository replyRepository;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    String id = "adasdd21331";
+    String title = "test01";
+    String comp = "donga";
+    String editor = "abc";
+    String keyword = "moon";
+    LocalDateTime time = LocalDateTime.of(2021, 10, 1, 0, 0, 0);
 
-    @After
-    public void cleanUp() {
-        repository.deleteAll();
-    }
+    String replyBody = "looooooooooooooooool";
+    String replyKeyword = "mun";
 
     @Transactional
     @Test
-    public void save_article(){
-        String id="adasdd21331";
-        String title="test01";
-        String comp="donga";
-        String editor="abc";
-        String keyword="moon";
-        LocalDateTime time=LocalDateTime.of(2021,10,1,0,0,0);
+    public void save_article() {//아무 연관 관계 없이 저장 테스트.
 
         //given
-        Article article=new Article.Builder()
+        Article article = new Article.Builder()
             .id(id)
             .title(title)
             .comp(comp)
@@ -52,12 +52,13 @@ public class ArticleRepositoryTest {
             .dateTime(time)
             .build();
 
-        repository.save(article);
+        articleRepository.save(article);
+
         //when
-        List<Article>articleList=repository.findAll();
+        List<Article> articleList = articleRepository.findAll();
 
         //then
-        Article a=articleList.get(0);
+        Article a = articleList.get(0);
         logger.info(a.toString());
         assertThat(a.getTitle()).isEqualTo(article.getTitle());
         assertThat(a.getComp()).isEqualTo(article.getComp());
@@ -67,19 +68,56 @@ public class ArticleRepositoryTest {
 
     @Transactional
     @Test
-    public void save_article_with_replies(){
+    public void save_article_with_replies() {//기사에 답글 연관관계 추가해서 엔티티 저장 테스트.
+        //given
+        Article article = new Article.Builder()
+            .id(id)
+            .title(title)
+            .comp(comp)
+            .editor(editor)
+            .keyWord(keyword)
+            .dateTime(time)
+            .build();
+
+        articleRepository.save(article);
+
+        Reply reply = new Reply.Builder()
+            .body(replyBody)
+            .keyWord(replyKeyword)
+            .dateTime(time)
+            .build();
+
+        reply.setArticle(article);
+        replyRepository.save(reply);
+
+        //when
+        Reply foundReply=replyRepository.findAll().get(0);
+
+        //then
+        logger.info("article"+article.toString());
+        logger.info("reply"+reply.toString()+reply.getArticle().toString());
+
+        assertThat(foundReply.getBody()).isEqualTo(reply.getBody());
+        assertThat(foundReply.getKeyWord()).isEqualTo(reply.getKeyWord());
+        assertThat(foundReply.getDate()).isEqualTo(reply.getDate());
+
+        assertThat(foundReply.getArticle().getTitle()).isEqualTo(article.getTitle());
+        assertThat(foundReply.getArticle().getComp()).isEqualTo(article.getComp());
+        assertThat(foundReply.getArticle().getEditor()).isEqualTo(article.getEditor());
+        assertThat(foundReply.getArticle().getKeyWord()).isEqualTo(article.getKeyWord());
+        assertThat(foundReply.getArticle().getDate()).isEqualTo(article.getDate());
 
     }
 
     @Transactional
     @Test
-    public void save_article_with_emojies(){
+    public void save_article_with_emojies() {
 
     }
 
     @Transactional
     @Test
-    public void save_article_with_replies_and_emojies(){
+    public void save_article_with_replies_and_emojies() {
 
     }
 
