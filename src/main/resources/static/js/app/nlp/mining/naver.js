5d462defcbe35773c43230c3f147e53ab8733320
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-10-17 15:54:44 +0900

    캡션 추가

--- src/main/resources/static/js/app/nlp/mining/naver.js
+++ src/main/resources/static/js/app/nlp/mining/naver.js
@@ -17,13 +17,19 @@ const naverArrow={
         type: "GET",
         url: "/api/nlp/mine/naver/"+value,
         success: function(response) {
+
               $("#naverContent").empty();
               $("#naverContent").html(response);
 
+              $('#naverTitle').empty();
+              $("#naverTitle").append("<h1>LDA 모델링 차트</h1>");
+
               $('#naverComp').empty();
+              $("#naverComp").append("<h1>타 사이트와의 비교</h1>");
               $('#naverComp').append('<img src="/images/'+value+'.png" style="height: auto; width: auto; max-width: 1200px; max-height: 500px;">');
 
               $('#naverWord').empty();
+              $('#naverWord').append("<h1>워드 클라우드</h1>");
               $("#naverWord").append('<img src="/images/Ncloud'+value+'.png" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
               $("#naverWord").append('<img src="/images/sometrend'+value+'.png" style="padding-left:10px;height: auto; width: auto; max-width: 580px; max-height: 500px;">');
         }
--- src/main/resources/static/js/app/nlp/mining/twitter.js
+++ src/main/resources/static/js/app/nlp/mining/twitter.js
@@ -18,12 +18,17 @@ const twitterArrow={
         url: "/api/nlp/mine/twitter/"+value,
         success: function(response) {
               $("#twitterContent").empty();
+
               $("#twitterContent").html(response);
+              $('#twitterTitle').empty();
+              $("#twitterTitle").append("<h1>LDA 모델링 차트</h1>");
 
               $('#twitterComp').empty();
+              $("#twitterComp").append("<h1>타 사이트와의 비교</h1>");
               $('#twitterComp').append('<img src="/images/'+value+'.png" style="height: auto; width: auto; max-width: 1200px; max-height: 500px;">');
 
               $('#twitterWord').empty();
+              $('#twitterWord').append("<h1>워드 클라우드</h1>");
               $("#twitterWord").append('<img src="/images/cloud'+value+'.png" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
               $("#twitterWord").append('<img src="/images/sometrend'+value+'.png" style="padding-left:10px;height: auto; width: auto; max-width: 580px; max-height: 500px;">');
 
