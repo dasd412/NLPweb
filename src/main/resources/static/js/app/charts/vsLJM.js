fcea7daeba331fadfe3f4bc85f353963e40817c5
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-11-04 18:39:25 +0900

    하드 코딩해서 vs 이재명 끝내긴 함.. if문이 너무 많이 나와서 지저분한게 맘에 안든다.

--- src/main/resources/static/js/app/charts/vsLJM.js
+++ src/main/resources/static/js/app/charts/vsLJM.js
@@ -56,6 +56,7 @@ function changeDateAndCandidate(){
   //read Value of select boxes
   let dateSelected=$("#info_date_vsLJM option:selected").val();
   let candidate=$("#candidate option:selected").val();
+  let sourceSelected=$("#info_source_vsLJM option:selected").val();
 
   //reInit
   chart.data.labels=[];
@@ -70,30 +71,48 @@ function changeDateAndCandidate(){
   if (dateSelected=='09210928'){
     if(candidate=='candidate_YSY'){
       chart.data.labels=['윤석열','이재명'];
-      dataInserted=[45.1, 54.8];
+      if (sourceSelected=='naver'){
+        dataInserted=[45.1, 54.8];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[5.2, 94.9];
+      }else if (sourceSelected=='both'){
+        dataInserted=[22.0, 77.9];
+      }
     }else if (candidate=='candidate_HJP'){
       chart.data.labels=['홍준표','이재명'];
-      dataInserted=[38.2, 61.8];
-    }else if (candidate=='candidate_WHR'){
-
-    }else if (candidate=='candidate_RSM'){
-
+      if (sourceSelected=='naver'){
+        dataInserted=[38.2, 61.8];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[3.2, 96.7];
+      }else if (sourceSelected=='both'){
+        dataInserted=[17.1, 82.9];
+      }
     }
     chart.data.datasets.forEach((dataset)=>{
        dataset.label='0921 - 0928';
        dataset.data=dataInserted;
     });
-  }else if (dateSelected=='09281005'){
+  }
+  else if (dateSelected=='09281005'){
     if(candidate=='candidate_YSY'){
       chart.data.labels=['윤석열','이재명'];
-      dataInserted=[42.4, 57.5];
-    }else if (candidate=='candidate_HJP'){
+      if (sourceSelected=='naver'){
+        dataInserted=[42.4, 57.5];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[42.4, 57.5];
+      }
+    }
+    else if (candidate=='candidate_HJP'){
       chart.data.labels=['홍준표','이재명'];
-      dataInserted=[33.6, 66.6];
-    }else if (candidate=='candidate_WHR'){
+      if (sourceSelected=='naver'){
+       dataInserted=[33.6, 66.6];
+      }else if (sourceSelected=='twitter'){
 
-    }else if (candidate=='candidate_RSM'){
-
+      }else if (sourceSelected=='both'){
+       dataInserted=[33.6, 66.6];
+      }
     }
     chart.data.datasets.forEach((dataset)=>{
            dataset.label='0928 - 1005';
@@ -102,16 +121,40 @@ function changeDateAndCandidate(){
   }else if (dateSelected=='10051012'){
     if(candidate=='candidate_YSY'){
       chart.data.labels=['윤석열','이재명'];
-      dataInserted=[43.3, 56.7];
+      if (sourceSelected=='naver'){
+        dataInserted=[43.3, 56.7];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[43.3, 56.7];
+      }
     }else if (candidate=='candidate_HJP'){
       chart.data.labels=['홍준표','이재명'];
-      dataInserted=[37.0, 63.7];
+      if (sourceSelected=='naver'){
+        dataInserted=[37.0, 63.7];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[37.0, 63.7];
+      }
     }else if (candidate=='candidate_WHR'){
       chart.data.labels=['원희룡','이재명'];
-      dataInserted=[30.3, 70.0];
+      if (sourceSelected=='naver'){
+        dataInserted=[30.3, 70.0];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[30.3, 70.0];
+      }
     }else if (candidate=='candidate_RSM'){
       chart.data.labels=['유승민','이재명'];
-      dataInserted=[35.8, 64.2];
+      if (sourceSelected=='naver'){
+        dataInserted=[35.8, 64.2];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[35.8, 64.2];
+      }
     }
     chart.data.datasets.forEach((dataset)=>{
            dataset.label='1005 - 1012';
@@ -120,16 +163,40 @@ function changeDateAndCandidate(){
   }else if (dateSelected=='10121019'){
     if(candidate=='candidate_YSY'){
       chart.data.labels=['윤석열','이재명'];
-      dataInserted=[46.4, 53.6];
+      if (sourceSelected=='naver'){
+        dataInserted=[46.4, 53.6];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[46.4, 53.6];
+      }
     }else if (candidate=='candidate_HJP'){
       chart.data.labels=['홍준표','이재명'];
-      dataInserted=[36.9, 63.6];
+      if (sourceSelected=='naver'){
+        dataInserted=[36.9, 63.6];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[36.9, 63.6];
+      }
     }else if (candidate=='candidate_WHR'){
       chart.data.labels=['원희룡','이재명'];
-      dataInserted=[33.5, 67.1];
+      if (sourceSelected=='naver'){
+        dataInserted=[33.5, 67.1];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[33.5, 67.1];
+      }
     }else if (candidate=='candidate_RSM'){
       chart.data.labels=['유승민','이재명'];
-      dataInserted=[34.6, 65.7];
+      if (sourceSelected=='naver'){
+        dataInserted=[34.6, 65.7];
+      }else if (sourceSelected=='twitter'){
+
+      }else if (sourceSelected=='both'){
+        dataInserted=[34.6, 65.7];
+      }
     }
     chart.data.datasets.forEach((dataset)=>{
            dataset.label='1012 - 1019';
@@ -138,16 +205,40 @@ function changeDateAndCandidate(){
   }else if (dateSelected=='10191029'){
     if(candidate=='candidate_YSY'){
       chart.data.labels=['윤석열','이재명'];
-      dataInserted=[46.1, 53.9];
+      if (sourceSelected=='naver'){
+        dataInserted=[46.1, 53.9];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[14.5, 85.5];
+      }else if (sourceSelected=='both'){
+        dataInserted=[29.5, 70.5];
+      }
     }else if (candidate=='candidate_HJP'){
       chart.data.labels=['홍준표','이재명'];
-      dataInserted=[36.7, 63.6];
+      if (sourceSelected=='naver'){
+        dataInserted=[36.7, 63.6];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[12.1, 88.0];
+      }else if (sourceSelected=='both'){
+        dataInserted=[23.0, 77.3];
+      }
     }else if (candidate=='candidate_WHR'){
-      chart.data.labels=['원희룡','이재명'];
-      dataInserted=[33.1, 66.9];
+       chart.data.labels=['원희룡','이재명'];
+      if (sourceSelected=='naver'){
+        dataInserted=[33.1, 66.9];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[8.1, 91.9];
+      }else if (sourceSelected=='both'){
+        dataInserted=[19.1, 80.9];
+      }
     }else if (candidate=='candidate_RSM'){
       chart.data.labels=['유승민','이재명'];
-      dataInserted=[35.8, 64.4];
+      if (sourceSelected=='naver'){
+        dataInserted=[35.8, 64.4];
+      }else if (sourceSelected=='twitter'){
+        dataInserted=[4.3, 96.6];
+      }else if (sourceSelected=='both'){
+        dataInserted=[18.4, 81.8];
+      }
     }
     chart.data.datasets.forEach((dataset)=>{
            dataset.label='1019 - 1029';
