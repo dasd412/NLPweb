de40dc368153514626ea8d0f007fb2551aacbf02
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-11-04 13:57:51 +0900

    국힘 4인 구도 차트 만듬. 단, 현재 네이버만 확인할 수 있음. 추후 다른 출처도 넣어야 함.

--- src/main/resources/static/js/app/charts/vs4.js
+++ src/main/resources/static/js/app/charts/vs4.js
@@ -1,28 +1,21 @@
 const config = {
-  type: 'bar',
+  type: 'horizontalBar',
   data: {
-          labels: ['윤석열', '이낙연', '이재명', '홍준표', '황교안'],
+          labels: ['홍준표','윤석열','원희룡','유승민'],
           datasets: [
-                  {
-                    label: '네이버',
-                    data: [0.34580631291531067, 0.1327654540044166, 0.24482311771410395, 0.21221944615281246, 0.06619936450469908],
-                    backgroundColor:'rgba(255, 99, 132, 0.5)'
-                  },
-                  {
-                    label:'트위터',
-                    data:[0.07138418006439708, 0.36745286561146473, 0.5668638690335713, 0.03344319941284449, 0.0016029807531660431],
-                    backgroundColor:'rgba(54, 162, 235, 0.5)'
-                  },
-                  {
-                    label:'네이터&트위터',
-                    data:[0.18805215685950696, 0.2608740248763238, 0.42899898184340857, 0.10909401670725122, 0.02919114681052689],
-                    backgroundColor:'rgba(255, 206, 86, 0.5)'
-                  },
-                  {
-                    label:'통계자료',
-                    data:[],
-                    backgroundColor:'rgba(75, 192, 192, 0.5)'
-                  }
+              {
+                        label:'0921 - 0928',
+                        data:[0.42859946030196766, 0.5719065276912119,0,0],
+                        fill:false,
+                         backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)'
+                                        ],
+                        tension: 0.2
+              },
+
 
           ]
   },
@@ -30,7 +23,7 @@ const config = {
   options:{
     title:{
       display:true,
-      text:'지지율 차트',
+      text:'국민의 힘 가상 대결',
       fontSize:20,
       fontColor:'rgba(46, 49, 49, 1)'
     },
@@ -39,40 +32,100 @@ const config = {
                  fontColor: 'rgba(83, 51, 237, 1)',
                  fontSize: 15
              }
-    }
+    },
+    scales: {
+                yAxes: [{
+                    ticks: {
 
+                        position: 'right',
+                        beginAtZero:true
+                    }
+                }]
+            }
   }
 };
-
-const barManipulator={
-
-  init:function(){
-    const _this=this;
-    $('#info_date').on('click',function(){
-          _this.get_info_date();
-    });
-  },//init function
-  get_info_date:function(){
-
-  }
-
-};
-
-//'rgba(255, 99, 132, 0.5)',  'rgba(54, 162, 235, 0.5)','rgba(255, 206, 86, 0.5)','rgba(75, 192, 192, 0.5)',
-
-
-//0.34580631291531067, 0.1327654540044166, 0.24482311771410395, 0.21221944615281246, 0.06619936450469908
-
-//#트위터 지지율 09.21 - 09.27
-//0.07138418006439708, 0.36745286561146473, 0.5668638690335713, 0.03344319941284449, 0.0016029807531660431
-
-//#네이버&트위터 통합 지지율 09.21 - 09.27
-//0.18805215685950696, 0.2608740248763238, 0.42899898184340857, 0.10909401670725122, 0.02919114681052689
-
 
 const ctx = document.getElementById('chart').getContext('2d');
 const chart = new Chart(ctx, config);
 
+function changeDateAndSource(){
+  //read Value of select boxes
+  let dateSelected=$("#info_date_vs4 option:selected").val();
+  let sourceSelected=$("#info_source_vs4 option:selected").val();
+
+  //reInit
+  chart.data.datasets.forEach((dataset) => {
+          dataset.label='';
+          dataset.data=[];
+  });
+
+  dataInserted=[];
+
+  //data & source case
+  if (dateSelected=='09210928'){
+    if(sourceSelected=='naver'){
+      dataInserted=[0.42859946030196766, 0.5719065276912119,0,0];
+    }else if (sourceSelected=='twitter'){
+
+    }else if (sourceSelected=='realMeter'){
+
+    }
+    chart.data.datasets.forEach((dataset)=>{
+       dataset.label='0921 - 0928';
+       dataset.data=dataInserted;
+    });
+  }else if (dateSelected=='09281005'){
+     if(sourceSelected=='naver'){
+       dataInserted=[0.4075637354231771, 0.5951261092091393,0,0];
+     }else if (sourceSelected=='twitter'){
+
+     }else if (sourceSelected=='realMeter'){
+
+     }
+    chart.data.datasets.forEach((dataset)=>{
+           dataset.label='0928 - 1005';
+           dataset.data=dataInserted;
+    });
+  }else if (dateSelected=='10051012'){
+       if(sourceSelected=='naver'){
+         dataInserted=[0.2492525540170098, 0.32824564077951024, 0.1860864515562396, 0.23930070510532286];
+       }else if (sourceSelected=='twitter'){
+
+       }else if (sourceSelected=='realMeter'){
+
+       }
+    chart.data.datasets.forEach((dataset)=>{
+           dataset.label='1005 - 1012';
+           dataset.data=dataInserted;
+    });
+  }else if (dateSelected=='10121019'){
+       if(sourceSelected=='naver'){
+         dataInserted=[0.23513195979659232, 0.35116614164617965, 0.2023908070868668, 0.21294505159352045];
+       }else if (sourceSelected=='twitter'){
+
+       }else if (sourceSelected=='realMeter'){
+
+       }
+    chart.data.datasets.forEach((dataset)=>{
+           dataset.label='1012 - 1019';
+           dataset.data=dataInserted;
+    });
+  }else if (dateSelected=='10191029'){
+       if(sourceSelected=='naver'){
+         dataInserted=[0.23288472687788686, 0.34461709255223844, 0.19975605065057855, 0.2240048449758903];
+       }else if (sourceSelected=='twitter'){
+
+       }else if (sourceSelected=='realMeter'){
+
+       }
+    chart.data.datasets.forEach((dataset)=>{
+           dataset.label='1019 - 1029';
+           dataset.data=dataInserted;
+    });
+  }
+  chart.update();
+}
+
 $(document).ready(function () {
-  barManipulator.init();
+  $('#vs4Btn').attr('onclick', 'changeDateAndSource()');
 });
