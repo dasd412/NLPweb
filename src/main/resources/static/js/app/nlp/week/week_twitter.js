42a47208d7c40a3001d567e1b4fc22244e09af83
yein-lee <agop0310@gmail.com>
2021-11-28 18:19:33 +0900

    1.테스트 디렉토리 삭제(다른 곳에서 빌드 시 오류 발생하므로) 2.트위터 주 차별 워드 클라우드 업로드.

--- src/main/resources/static/js/app/nlp/week/week_twitter.js
+++ src/main/resources/static/js/app/nlp/week/week_twitter.js
@@ -2,6 +2,12 @@
 let latest_week_twitter="";
 const week_twitter_dict={0:"week1_T_",4:"week5_T_",5:"week6_T_"};
 
+function imgError(image) {
+    image.onerror = "";
+    image.src = "/images/notFound.png";
+    return true;
+}
+
 const twitterWeekArrow={
 
   init:function(){
@@ -13,6 +19,7 @@ const twitterWeekArrow={
     $("#HJP_week_T").on('click',function(){_this.selectTwitterResult("HJP");});
     $("#WHR_week_T").on('click',function(){_this.selectTwitterResult("WHR");});
     $("#RSM_week_T").on('click',function(){_this.selectTwitterResult("RSM");});
+    $("#ACS_week_T").on('click',function(){_this.selectTwitterResult("ACS");});
   },//init
 
   selectTwitterResult : function(value){
@@ -21,9 +28,13 @@ const twitterWeekArrow={
         $(latest_week_twitter).css("color","black");
     }
 
+    const selected_week=$("#twitter_week_select option:selected").val();
+    const selected=week_twitter_dict[selected_week]+value;
+    console.log(selected);
+
     $('#twitter_word_week').empty();
     $('#twitter_word_week').append("<h1>주차별 후보의 트위터 워드 클라우드</h1>");
-    $("#twitter_word_week").append('<img src="/images/'+selected+'.png" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
+    $("#twitter_word_week").append('<img src="/images/'+selected+'.png" onerror="imgError(this)" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
 
     const target="#"+value+"_week_T";
 
