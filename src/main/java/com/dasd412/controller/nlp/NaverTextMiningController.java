672a6a457db5795b3fc9b557df91cad5a39b3b45
yein-lee <agop0310@gmail.com>
2021-11-28 17:38:01 +0900

    네이버 lda 결과 동적으로 볼 수 있도록 완료함.

--- src/main/java/com/dasd412/controller/nlp/NaverTextMiningController.java
+++ src/main/java/com/dasd412/controller/nlp/NaverTextMiningController.java
@@ -1,10 +1,14 @@
 package com.dasd412.controller.nlp;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class NaverTextMiningController {
@@ -64,5 +68,16 @@ public class NaverTextMiningController {
     public String viewResolveWeekNaver(Model model) {
         logger.info("week naver view");
         return "/week_naver/naver";
+    }
+
+    //주차별 네이버 lda 결과 뷰 반환
+    @GetMapping("/api/nlp/week/naver/LDA")
+    public String getLDANaverViewResolve(@RequestParam Map<String, String> params) {
+        logger.info("get params of lda naver : " + params);
+        String folderName=params.get("folder");
+        String candidate=params.get("candidate");
+        logger.info("week nave lda view resolve"+folderName+candidate);
+        logger.info("LDANaver/"+folderName+"/"+candidate);
+        return "LDANaver/"+folderName+"/"+candidate;
     }
 }
--- src/main/resources/static/js/app/nlp/week/week_naver.js
+++ src/main/resources/static/js/app/nlp/week/week_naver.js
@@ -2,7 +2,8 @@
 let latest_week_naver="";
 const week_naver_dict={0:"week1_N_",1:"week2_N_",2:"week3_N_",
 3:"week4_N_",4:"week5_N_",5:"week6_N_"};
-
+const lda_naver_dict={0:"09220926",1:"10051008",2:"10121015",
+                     3:"10181022",4:"10251029",5:"11011105"};
 function imgError(image) {
     image.onerror = "";
     image.src = "/images/notFound.png";
@@ -35,12 +36,19 @@ const naverWeekArrow={
     $('#naver_word_week').append("<h1>주차별 후보의 네이버 워드 클라우드</h1>");
     $("#naver_word_week").append('<img src="/images/'+selected+'.png" onerror="imgError(this)" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
 
+    const params={
+        folder:lda_naver_dict[selected_week],
+        candidate:value
+    };
+
+    $("#naver_word_content").empty();
+
     $.ajax({
         type: "GET",
-        url: "/api/nlp/mine/naver/MJI",
+        url: "/api/nlp/week/naver/LDA",
+        data:params,
+        contentType:'application/json;charset=utf-8',
         success: function(response) {
-
-              $("#naver_word_content").empty();
               $("#naver_word_content").html(response);
         }
     });
