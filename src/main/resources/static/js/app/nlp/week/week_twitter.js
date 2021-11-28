9ddb67dac6675a0315a4d9f1e39e4a643895b20c
yein-lee <agop0310@gmail.com>
2021-11-28 18:47:04 +0900

    LDA 정적 HTML 파일 업로드

--- src/main/resources/static/js/app/nlp/week/week_twitter.js
+++ src/main/resources/static/js/app/nlp/week/week_twitter.js
@@ -1,7 +1,8 @@
 
 let latest_week_twitter="";
 const week_twitter_dict={0:"week1_T_",4:"week5_T_",5:"week6_T_"};
-
+const lda_twitter_dict={0:"09220926",1:"10051008",2:"10121015",
+                     3:"10181022",4:"10251029",5:"11011105"};
 function imgError(image) {
     image.onerror = "";
     image.src = "/images/notFound.png";
@@ -30,11 +31,27 @@ const twitterWeekArrow={
 
     const selected_week=$("#twitter_week_select option:selected").val();
     const selected=week_twitter_dict[selected_week]+value;
-    console.log(selected);
 
     $('#twitter_word_week').empty();
     $('#twitter_word_week').append("<h1>주차별 후보의 트위터 워드 클라우드</h1>");
     $("#twitter_word_week").append('<img src="/images/'+selected+'.png" onerror="imgError(this)" style="height: auto; width: auto; max-width: 580px; max-height: 500px;">');
+
+    const params={
+            folder:lda_twitter_dict[selected_week],
+            candidate:value
+    };
+
+    $("#twitter_word_content").empty();
+
+    $.ajax({
+        type: "GET",
+        url: "/api/nlp/week/twitter/LDA",
+        data:params,
+        contentType:'application/json;charset=utf-8',
+        success: function(response) {
+              $("#twitter_word_content").html(response);
+        }
+    });
 
     const target="#"+value+"_week_T";
 
